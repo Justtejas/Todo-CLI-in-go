283e6443758fb13400319ff9be270c39004b9e1c
Justtejas <tejaspantoji@gmail.com>
2023-11-18 23:32:48 +0530

    Completed Add flag and the Todo Logic

--- todo/main.go
+++ todo/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	todo "github.com/justtejas/Todo-CLI-in-go"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
-	fmt.Print("To do in go")
+	add := flag.Bool("add", false, "add a new task")
+	flag.Parse()
+
+	todos := &todo.Todo{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch{
+	case *add:
+		todos.Add("Just a test")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Println("Error")
+			os.Exit(1)
+		} 
+	default:
+		fmt.Println("Invalid Command")
+		os.Exit(0)
+	}
 }
