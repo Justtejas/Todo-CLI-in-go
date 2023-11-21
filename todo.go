ad695d05bf932fd142c6f408df399409fbe4c858
Justtejas <tejaspantoji@gmail.com>
2023-11-21 16:45:23 +0530

    Done with all the flags

--- todo.go
+++ todo.go
@@ -102,11 +102,11 @@ func (t *Todo) Print()  {
 
 	for idx, item := range *t {
 		idx++
-		task := blue(item.Task)
-		done := blue("No")
+		task := item.Task
+		done := "No"
 		if item.Done {
-			task = green(fmt.Sprintf("\u2705 %s", item.Task))
-			done = green("Yes")
+			task = fmt.Sprintf("\u2705 %s", item.Task)
+			done = "Yes"
 		}
 		cells = append(cells, []*simpletable.Cell{
 			{Text: fmt.Sprintf("%d", idx)},
@@ -120,7 +120,7 @@ func (t *Todo) Print()  {
 	table.Body = &simpletable.Body{Cells: cells}
 
 	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
-		{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("You have %d pending todos", t.CountPending()))},
+		{Align: simpletable.AlignCenter, Span: 5, Text: fmt.Sprintf("You have %d pending task(s)", t.CountPending())},
 	}}
 
 	table.SetStyle(simpletable.StyleUnicode)
